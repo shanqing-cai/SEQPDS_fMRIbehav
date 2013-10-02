c30595c10a61e8a3308a7a82e1303232557a3747
shanqing-cai <shanqing.cai@gmail.com>
2013-10-02 16:10:26 -0400

    Fixed bugs in tracking of previos fmtOpts etc.
    Update ALPHA_DEFAULT
    
    Added display of detailed trial infomation in the trial list window

--- updateTrialList.m
+++ updateTrialList.m
@@ -6,10 +6,26 @@ for i1 = 1 : length(data)
     else
         completionStr = '_';
     end
-    
+        
     listItems{i1} = sprintf('[%s] %.3d - %s', completionStr, i1, data{i1}.stimWord);
+    
+    if data{i1}.fluency > 1
+        listItems{i1} = [listItems{i1}, ' (Disfluency)'];
+    end
+    
+    if data{i1}.accuracy > 1
+        listItems{i1} = [listItems{i1}, ' (Inaccurate)'];
+    end
+    
+    if isfield(data{i1}, 'fmtOpts') ...
+       && isfield(data{i1}, 'f0_time') && ~isempty(data{i1}.f0_time) ...
+       && isfield(data{i1}, 'f0') && ~isempty(data{i1}.f0) ...
+       && isfield(data{i1}, 'fmt_time') && ~isempty(data{i1}.fmt_time) ...
+       && isfield(data{i1}, 'f1') && ~isempty(data{i1}.f1) ...
+       && isfield(data{i1}, 'f2') && ~isempty(data{i1}.f2);
+        listItems{i1} = [listItems{i1}, ' [F0,Fmt done]'];
+    end
 end
-
 set(uihdls.trialListBox, 'string', listItems);
 
 %% Set current focus to the next unfinished trial
